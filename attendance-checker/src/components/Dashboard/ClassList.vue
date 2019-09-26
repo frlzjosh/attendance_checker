8995df94ae2c734b7499db0f49f0cc2fbfc6dbf0
jsc31994 <jsc31994@gmail.com>
2019-09-26 10:58:53 -0700

    Created state and component for Class List.

---
+++ attendance-checker/src/components/Dashboard/ClassList.vue
@@ -0,0 +1,27 @@
+<template>
+<table>
+    <tr>
+        <th>Course</th>
+        <th>Section</th>
+        <th>Class Times</th>
+    </tr>
+    <tr v-for="row in courseList">
+        <td>{{row.course}}</td>
+        <td>{{row.section}}</td>
+        <td>{{row.classTimes}}</td>
+    </tr>
+</table>    
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    computed: {
+        ...mapState
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ attendance-checker/src/store/modules/classes/state.js
@@ -0,0 +1,22 @@
+export default {
+    courseList: [
+        {
+            course: "CIT 480/L",
+            section: "16669/16670",
+            classTime: "9:30AM-10:45AM/10:50AM-11:30AM"
+        },
+
+        {
+            course: "CIT 160/L",
+            section: "17020/17022",
+            classTime: "11:00AM-11:50AM/12:30PM-01:45PM"
+        },
+
+        {
+            course: "COMP 122/L",
+            section: "16793/16794",
+            classTime: "03:00PM-03:25PM/03:30PM-04:45PM"
+        }
+    ],
+
+}
